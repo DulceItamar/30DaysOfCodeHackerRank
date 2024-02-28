51da84c763770563be4738edf282487ef0ca30c7
dulceitamar <dulceitamar22@gmail.com>
2024-02-28 13:27:42 -0600

    Day4: Class vs Instance

---
+++ Day4_ Class-vs-Instance.playground/Contents.swift
@@ -0,0 +1,63 @@
+import UIKit
+
+/*
+ 
+ Write a Person class with an instance variable, , and a constructor that takes an integer, , as a parameter. The constructor must assign  to  after confirming the argument passed as  is not negative; if a negative argument is passed as , the constructor should set  to  and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+
+ yearPasses() should increase the  instance variable by .
+ amIOld() should perform the following conditional actions:
+ If , print You are young..
+ If  and , print You are a teenager..
+ Otherwise, print You are old..
+ To help you learn by example and complete this challenge, much of the code is provided for you, but you'll be writing everything in the future. The code that creates each instance of your Person class is in the main method. Don't worry if you don't understand it all quite yet!
+ */
+
+
+class Person {
+    var age: Int = 0
+
+    init(initialAge: Int) {
+        // Add some more code to run some checks on initialAge
+        
+        if initialAge > 0 {
+            self.age = initialAge
+        } else {
+            self.age = 0
+            print("Age is not valid, setting age to 0.")
+        }
+        
+    }
+
+    func amIOld() {
+        // Do some computations in here and print out the correct statement to the console
+        if self.age < 13 {
+            print("You are young.")
+        } else if self.age >= 13 && self.age < 18 {
+            print("You are a teenager.")
+        } else {
+            print("You are old.")
+        }
+    }
+
+    func yearPasses() {
+        // Increment the age of the person in here
+        self.age += 1
+    }
+}
+
+let t = Int(readLine()!)!
+
+for _ in 0..<t {
+    let age = Int(readLine()!)!
+    let p = Person(initialAge: age)
+
+    p.amIOld()
+
+    for i in 1...3 {
+        p.yearPasses()
+    }
+
+    p.amIOld()
+
+    print("")
+}
